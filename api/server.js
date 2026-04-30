400146f94cfb5b9eb737e750f43780c34cdc8336
pvst07 <pvs.tw07@gmail.com>
2026-04-30 20:09:03 +0700

    minimal config for success

--- api/server.js
+++ api/server.js
@@ -7,11 +7,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const path = require('path');
 
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
-app.use(express.static(path.join(__dirname, '../public')));
 
 const chatLimiter = rateLimit({
   windowMs: 60 * 1000,
